99bebf654ef5f8666417c0f008fde500937eee10
Glavar <hellatriplexxx@gmail.com>
2026-06-11 00:40:06 +0300

    task 1,2,3,4 in noethwind db.

--- northwind/block_2_joins.sql
+++ northwind/block_2_joins.sql
@@ -1,7 +1,7 @@
 -- Задача 1. Все заказы + компания + страна --
 SELECT order_id, company_name, country
 FROM orders INNER JOIN customers
-USING customer_id;
+USING (customer_id);
 
 -- Задача 2. Имя клиента + кол-во заказов --
 SELECT customer_id, contact_name, count(order_id) AS count_orders
@@ -15,3 +15,15 @@ ORDER BY count_orders;
 SELECT e.first_name AS employee, m.first_name AS manager
 FROM employees e
 LEFT JOIN employees m ON e.reports_to = m.employee_id; 
+
+-- Задача 4. Топ 5 компаний с самой большой выручкой за 1997 год --
+
+SELECT company_name, SUM(quantity * unit_price) AS revenue
+FROM customers
+INNER JOIN orders USING (customer_id)
+INNER JOIN order_details ON orders.order_id = order_details.order_id
+WHERE EXTRACT(YEAR FROM order_date) = 1997
+GROUP BY customer_id, company_name
+ORDER BY revenue DESC
+LIMIT 5;
+
