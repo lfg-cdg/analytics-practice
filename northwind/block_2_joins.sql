dd64bb380ad019ad6f3b18858b8f22abd00d8d78
Glavar <hellatriplexxx@gmail.com>
2026-06-16 20:29:10 +0300

    новый раздел - оконные функции. new module - window functions

--- northwind/block_2_joins.sql
+++ northwind/block_2_joins.sql
@@ -27,3 +27,19 @@ GROUP BY customer_id, company_name
 ORDER BY revenue DESC
 LIMIT 5;
 
+|----------------------------------ОКОННЫЕ ФУНКЦИИ----------------------------------------|
+
+-- Задача 1. Вывести топ 3 цены по категориям, а также среднюю цену каждой категории --
+
+WITH ranked AS (
+    SELECT product_name, category_id, unit_price,
+           ROW_NUMBER() OVER (PARTITION BY category_id ORDER BY unit_price DESC) AS price_rank
+    FROM products
+)
+SELECT *, ROUND(AVG(unit_price) OVER (PARTITION BY category_id)::numeric, 2) AS avg_category_price FROM ranked
+WHERE price_rank <= 3
+ORDER BY category_id, price_rank
+;
+
+* Тип данных колонки unit_price - real, что не позволяет сразу обернуть среднюю цену в ROUND.
+  Для начала требуется перевести в нужный тип с помощью ::numeric
